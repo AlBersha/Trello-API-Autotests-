ad5d32ec7ea08c6000f07a62ca70bc444e60d1fc
Oleksandra Bershatska <oleksandra.bershatska@solarwinds.com>
2021-05-09 22:30:43 +0300

    test the pipeline

--- TrelloAutotests/BoardsTest.cs
+++ TrelloAutotests/BoardsTest.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using RA;
+﻿using RA;
 using Xunit;
-// using Microsoft.Extensions.Configuration;
 
 namespace TrelloAutotests
 {
@@ -20,17 +18,45 @@ namespace TrelloAutotests
                 .TestStatus("status", x => x == 200);
         }
         
-        // [Fact]
-        // public void GetNamesOfBoard_ShouldReturnListOfBoards()
-        // {
-        //     const string field = "name";
-        //     new RestAssured()
-        //         .Given()
-        //         .Header("Content-Type", "application/json")
-        //         .When()
-        //         .Get(Configs.BaseUri + Configs.BoardsUrl + $"?fields={field},url&key={Configs.Key}&token={Configs.Token}")
-        //         .Then()
-        //         .TestStatus("status", x => x == 200);
-        // }
+        [Fact]
+        public void GetNamesOfBoard_ShouldReturnListOfBoards()
+        {
+            const string field = "name";
+            new RestAssured()
+                .Given()
+                .Header("Content-Type", "application/json")
+                .When()
+                .Get(Configs.BaseUri + Configs.BoardsUrl + $"?fields={field},url&key={Configs.Key}&token={Configs.Token}")
+                .Then()
+                .TestStatus("status", x => x == 200);
+        }
+        
+        [Fact]
+        public void CreateLists_ShouldReturnNewList()
+        {
+            var boardId = "5dd85a7f8f92ff62c8a4fcd8"; // Daily tasks board
+            var listName = "Exams preparation";
+            new RestAssured()
+                .Given()
+                .Header("Content-Type", "application/json")
+                .When()
+                .Post(Configs.BaseUri + Configs.ListsUrl + $"?key={Configs.Key}&token={Configs.Token}&name={listName}&idBoard={boardId}")
+                .Then()
+                .TestStatus("status", x => x == 200);
+        }
+        
+        [Fact]
+        public void UpdateExistingLists_ShouldReturnUpdatedList()
+        {
+            var listId = "60981703c97b4a7aa03edd9f";
+            var listName = "Exams preparation 2021";
+            new RestAssured()
+                .Given()
+                .Header("Content-Type", "application/json")
+                .When()
+                .Put(Configs.BaseUri + Configs.ListsUrl + $"/{listId}?key={Configs.Key}&token={Configs.Token}&name={listName}")
+                .Then()
+                .TestStatus("status", x => x == 200);
+        }
     }
 }
--- TrelloAutotests/ListsTest.cs
+++ TrelloAutotests/ListsTest.cs
@@ -1,38 +1,12 @@
-﻿// using RA;
-// using Xunit;
-//
-// namespace TrelloAutotests
-// {
-//     public class ListsTest
-//     {
-//
-//         [Fact]
-//         public void CreateLists_ShouldReturnNewList()
-//         {
-//             var boardId = "5dd85a7f8f92ff62c8a4fcd8"; // Daily tasks board
-//             var listName = "Exams preparation";
-//             new RestAssured()
-//                 .Given()
-//                 .Header("Content-Type", "application/json")
-//                 .When()
-//                 .Post(Configs.BaseUri + Configs.ListsUrl + $"?key={Configs.Key}&token={Configs.Token}&name={listName}&idBoard={boardId}")
-//                 .Then()
-//                 .TestStatus("status", x => x == 200);
-//         }
-//         
-//         [Fact]
-//         public void UpdateExistingLists_ShouldReturnUpdatedList()
-//         {
-//             var listId = "60981703c97b4a7aa03edd9f";
-//             var listName = "Exams preparation 2021";
-//             new RestAssured()
-//                 .Given()
-//                 .Header("Content-Type", "application/json")
-//                 .When()
-//                 .Put(Configs.BaseUri + Configs.ListsUrl + $"/{listId}?key={Configs.Key}&token={Configs.Token}&name={listName}")
-//                 .Then()
-//                 .TestStatus("status", x => x == 200);
-//         }
-//         
-//     }
-// }
+﻿using RA;
+using Xunit;
+
+namespace TrelloAutotests
+{
+    public class ListsTest
+    {
+
+       
+        
+    }
+}
